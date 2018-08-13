71deb60e584833987ab23473ee40110b5262f57f
Lana <lanawo370@gmail.com>
2018-08-13 13:17:38 -0700

    There's now a date oon top of the order

--- app/src/main/java/com/example/lana/leahrestaurant/Order.java
+++ app/src/main/java/com/example/lana/leahrestaurant/Order.java
@@ -1,5 +1,6 @@
 package com.example.lana.leahrestaurant;
 
+import android.icu.util.Calendar;
 import android.support.annotation.VisibleForTesting;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Order extends AppCompatActivity {
     private static final String FILE_NAME = "orders.txt";
@@ -62,6 +64,11 @@ public class Order extends AppCompatActivity {
         });
 
         submitButton.setOnClickListener(view -> {
+            Date currentTime = Calendar.getInstance().getTime();
+            wholeOrder.add(0,String.valueOf(currentTime));
+            if(itemBlank != null){
+                addItem(wholeOrder);
+            }
             quantityNumber = 1;
             quantityTextView.setText("Quantity: " + quantityNumber);
             nameBlank.setVisibility(View.VISIBLE);
@@ -116,7 +123,7 @@ public class Order extends AppCompatActivity {
         try {
             fos = openFileOutput(FILE_NAME,MODE_APPEND);
             fos.write(text.getBytes());
-            Toast.makeText(this, "Saved to " + getFilesDir() + "/" + FILE_NAME,Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Saved to " + getFilesDir().getAbsolutePath() + "/" + FILE_NAME,Toast.LENGTH_LONG).show();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
