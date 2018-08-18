0d6cb255d30f61e1faac85fed7ffe87d9e3ccb82
Lana <lanawo370@gmail.com>
2018-08-18 10:58:55 -0700

    Text file now deletes when you haven't taken orders in a day, and recreates.

--- app/src/main/java/com/example/lana/leahrestaurant/Order.java
+++ app/src/main/java/com/example/lana/leahrestaurant/Order.java
@@ -1,23 +1,29 @@
 package com.example.lana.leahrestaurant;
 
 import android.icu.util.Calendar;
-import android.support.annotation.VisibleForTesting;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.logging.Logger;
 
 public class Order extends AppCompatActivity {
-    private static final String FILE_NAME = "orders.txt";
+
 
     private TextView quantityTextView;
     private Button addToQuantityButton;
@@ -30,6 +36,13 @@ public class Order extends AppCompatActivity {
     private Button submitButton;
     ArrayList<String> wholeOrder = new ArrayList<String>();
     int quantityNumber = 1;
+    int maxDays = 1;
+    boolean delete = false;
+
+    String CURRENT_FILE_NAME = "order.txt";
+    File current = new File("/data/user/0/com.example.lana.leahrestaurant/files/" + CURRENT_FILE_NAME);
+    FileOutputStream fos = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -104,39 +117,75 @@ public class Order extends AppCompatActivity {
         String personName = nameBlank.getText().toString();
         String order = itemBlank.getText().toString();
         ordered.add(personName);
-        ordered.add(quantityNumber +" "+ order);
+        if(order != null){
+            ordered.add(String.valueOf(quantityNumber) +" "+ order);
+        }else{
+            ordered.add("");
+        }
         return ordered;
     }
 
     private String puttingDataInAString(ArrayList<String> listOfItemsOrdered){
         String totalOrder = "";
+        listOfItemsOrdered.remove(listOfItemsOrdered.get(listOfItemsOrdered.size()-1));
         for(String item: listOfItemsOrdered){
             totalOrder = "\n" + totalOrder+ "\n" + item;
         }
         return totalOrder;
     }
 
-    private void saveToLocalStorage(String order){
-        String text = order;
-        FileOutputStream fos = null;
+    private void saveToLocalStorage(String order) {
 
-        try {
-            fos = openFileOutput(FILE_NAME,MODE_APPEND);
-            fos.write(text.getBytes());
-            Toast.makeText(this, "Saved to " + getFilesDir().getAbsolutePath() + "/" + FILE_NAME,Toast.LENGTH_LONG).show();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }finally {
-            if(fos != null){
-                try {
-                    fos.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+        //Dealing with time.
+        long lastModified = current.lastModified();
+        Date dt = new Date(lastModified);
+        long difference = System.currentTimeMillis() - dt.getTime();
+        long time = maxDays * 24 * 60 * 60 * 1000;
+        String text = order;
+
+
+        if (current.exists()) {
+            if(difference > time){
+                if(current.delete()){
+                    writeToSpecificFile(makeNewTextFile(), text);
+                    CURRENT_FILE_NAME = makeNewTextFile();
+                }else{
+                    Toast.makeText(this, "WTF", Toast.LENGTH_SHORT).show();
+                }
+            }else{
+                writeToSpecificFile(CURRENT_FILE_NAME, text);
+            }
+        }else{
+            writeToSpecificFile(makeNewTextFile(),text);
+            //Meaning the specified time didn't pass yet.
+        }
+
+    }
+
+    private String makeNewTextFile(){
+        String NEW_FILE_NAME = "order.txt";
+        return NEW_FILE_NAME;
+    }
+
+        public void writeToSpecificFile(String fileName, String textOrder){
+            try {
+                fos = openFileOutput(fileName,MODE_APPEND);
+                fos.write(textOrder.getBytes());
+                Toast.makeText(this, "Saved to " + getFilesDir().getAbsolutePath() + "/" + fileName,Toast.LENGTH_LONG).show();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                if(fos != null){
+                    try {
+                        fos.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
+
     }
 
-}
